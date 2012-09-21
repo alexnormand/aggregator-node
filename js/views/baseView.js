9001661996fb991aaa03e9cb8e6ede6dc75367e7
Alex Normand <anormnd@gmail.com>
2012-09-21 02:18:03 +0200

    site redesign #2

--- js/views/baseView.js
+++ js/views/baseView.js
@@ -7,13 +7,19 @@ define([
 
         loaderImage : '<div class="hero-unit loader"></div>',
 
-	      updateMainView: function(html, i) {
-	          var menuItems = $('ul.nav li');
+        updateMainView: function(html, i, footerAbsolutePosition) {
+            var menuItems = $('ul.nav li');
 
-	          $('#main').html(html);
-	          menuItems.removeClass('active');
-	          i !== null && menuItems.eq(i).addClass('active');
-	      }
+            $('#main').html(html);
+            menuItems.removeClass('active');
+            i !== null && menuItems.eq(i).addClass('active');
+
+            if (footerAbsolutePosition) {
+                $('body').addClass('footerAbsolute');
+            } else {
+                $('body').removeClass('footerAbsolute');
+            }
+        }
     });
 
     return BaseView;
